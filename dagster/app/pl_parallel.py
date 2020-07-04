592e91c26756f677ff343561cf3125d11ea49a3c
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2020-07-04 19:01:47 +0100

    fixes

--- dagster/app/pl_parallel.py
+++ dagster/app/pl_parallel.py
@@ -3,6 +3,7 @@ from dagster import ModeDefinition, default_executors, pipeline, solid, execute_
 import time
 
 
+
 celery_mode_defs = [ModeDefinition(executor_defs=default_executors + [celery_executor])]
 
 @solid
@@ -14,5 +15,6 @@ def not_much(context):
 
 @pipeline(mode_defs=celery_mode_defs)
 def parallel_pipeline():
+    
     for i in range(50):
         not_much.alias('not_much_' + str(i))()
